5402a7536054d53bd53f2fa07b1677e892bcab64
Keidbyr <117668622+Keidbyr@users.noreply.github.com>
2023-02-09 15:29:26 +0500

    1

--- main.py
+++ main.py
@@ -1,44 +1,80 @@
 import sys
-
+import json
+import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QLabel, QLineEdit
+from PyQt5.QtGui import QPixmap
+from API import API_KEY
 
 
 class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.maps_params = {
+            'll': '',
+            'size': '200,300',
+            'l': 'map',
+            'z': '8',
+            'spn': '0.005,0.005',
+            'pt': ''
+        }
 
     def initUI(self):
         self.setGeometry(300, 300, 400, 400)
         self.setWindowTitle('карты')
 
         self.text = QLabel("Координаты:", self)
-        self.text.move(0, 0)
+        self.text.move(10, 0)
+
+        self.text1 = QLabel("X", self)
+        self.text1.move(10, 30)
+
+        self.text2 = QLabel("Y", self)
+        self.text2.move(10, 50)
 
         self.ql1 = QLineEdit(self)
-        self.ql1.move(0, 30)
+        self.ql1.move(20, 25)
 
         self.ql2 = QLineEdit(self)
-        self.ql2.move(150, 30)
+        self.ql2.move(20, 50)
 
-        self.text1 = QLabel("маштаб:", self)
-        self.text1.move(0, 100)
+        self.text3 = QLabel("Маштаб:", self)
+        self.text3.move(10, 75)
 
-        self.ql3 = QLineEdit(self)
-        self.ql3.move(50, 100)
+        self.mashtab = QLineEdit(self)
+        self.mashtab.move(10, 100)
 
-        self.but = QPushButton("готово", self)
-        self.but.move(60, 150)
-        self.but.clicked.connect(self.run)
+        self.but = QPushButton("Готово", self)
+        self.but.move(30, 130)
+        self.but.clicked.connect(self.shower)
 
-    def run(self):
-        map_params = {
-            "ll": ",".join([int(self.ql1.text()), int(self.ql2.text())]),
-            "spn": ",".join([int(self.ql3.text()), int(self.ql3.text())]),
-            "l": "map"
-        }
-        self.map_api_server = "http://static-maps.yandex.ru/1.x/"
+        self.Error = QLabel("")
+        self.Error.move(20, 140)
+
+        self.label = QLabel(self)
+        self.label.size = 100
+        self.label.move(50, 140)
+
+    def shower(self):
+        self.maps_params['ll'] = ','.join([self.ql1.text(), self.ql2.text()])
+        if self.mashtab.text():
+            self.maps_params['spn'] = f'{self.mashtab.text()},{self.mashtab.text()}'
+        self.show_map()
+
+    def show_map(self):
+        try:
+            response = requests.get("https://static-maps.yandex.ru/1.x/",
+                                    params=self.maps_params)
+            with open("data/map.png", "wb") as file:
+                file.write(response.content)
+        except requests.exceptions.ConnectionError:
+            self.Error.setText('Connection failed')
+            self.Error.show()
+        else:
+            self.Error.hide()
+            self.pixmap = QPixmap('data/map.png')
+            self.label.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':
