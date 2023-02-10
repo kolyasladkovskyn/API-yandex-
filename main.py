7985694714dff442e7351e05b263f313e02f8496
Keidbyr <117668622+Keidbyr@users.noreply.github.com>
2023-02-10 10:17:12 +0500

    первые 3 задания(c фиксом 2)

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdi
 from PyQt5.QtGui import QPixmap
 from API import API_KEY
 
+sys._excepthook = sys.excepthook  # save original excepthook
 
-sys._excepthook = sys.excepthook # save original excepthook
 
 def exception_hook(exctype, value, traceback):
-    print(exctype, value, traceback) # print exception.
-    sys._excepthook(exctype, value, traceback) # call original excepthoot. I do not why
-    sys.exit(1) # terminate program if above do not do this
+    print(exctype, value, traceback)  # print exception.
+    sys._excepthook(exctype, value, traceback)  # call original excepthoot. I do not why
+    sys.exit(1)  # terminate program if above do not do this
 
 
 class Example(QWidget):
@@ -20,43 +20,62 @@ class Example(QWidget):
         self.initUI()
         self.maps_params = {
             'll': '',
-            'size': '400,200',
+            'size': '300,300',
             'l': 'map',
             'z': '8',
             'spn': '0.005,0.005',
             'pt': ''
         }
 
+    def check_y(self, y):
+        if y > 78.0:
+            return str(0.0)
+        elif y < 0:
+            return str(78.0)
+        return str(y)
+
+    def check_x(self, x):
+        if x < -179.0:
+            return str(179.0)
+        elif x > 179.0:
+            return str(-179.0)
+        return str(x)
+
     def keyPressEvent(self, event):
         key = event.key()
         super().keyPressEvent(event)
         # вверх с помощью W
         if key == 87:
-            x = str(float(self.maps_params['ll'].split(',')[1]) + 0.5)
-            self.maps_params['ll'] = (self.maps_params['ll'].split(',')[0]+',' + x)
+            y = float(self.maps_params['ll'].split(',')[1]) + 0.5
+            y = self.check_y(y)
+            self.maps_params['ll'] = (self.maps_params['ll'].split(',')[0] + ',' + y)
         # вниз с помощью S
         elif key == 83:
-            x = str(float(self.maps_params['ll'].split(',')[1]) - 0.5)
-            self.maps_params['ll'] = (self.maps_params['ll'].split(',')[0] + ',' + x)
+            y = float(self.maps_params['ll'].split(',')[1]) - 0.5
+            y = self.check_y(y)
+            self.maps_params['ll'] = (self.maps_params['ll'].split(',')[0] + ',' + y)
         # вправо с помощью D
         elif key == 68:
-            y = str(float(self.maps_params['ll'].split(',')[0]) + 0.5)
-            self.maps_params['ll'] = (y + ',' + self.maps_params['ll'].split(',')[1])
+            x = float(self.maps_params['ll'].split(',')[0]) + 2
+            x = self.check_x(x)
+            self.maps_params['ll'] = (x + ',' + self.maps_params['ll'].split(',')[1])
         # влево с помощью A
         elif key == 65:
-            y = str(float(self.maps_params['ll'].split(',')[0]) - 0.5)
-            self.maps_params['ll'] = (y + ',' + self.maps_params['ll'].split(',')[1])
+            x = float(self.maps_params['ll'].split(',')[0]) - 2
+            x = self.check_x(x)
+            self.maps_params['ll'] = (x + ',' + self.maps_params['ll'].split(',')[1])
         # вверх маштаб
         elif key == 16777238:
-            a = str(float(self.maps_params['spn'].split(',')[0])+9)
-            self.maps_params['spn'] = (a+','+a)
+            a = str(float(self.maps_params['spn'].split(',')[0]) + 5)
+            self.maps_params['spn'] = (a + ',' + a)
         # вниз маштаб
         elif key == 16777239:
-            a = str(float(self.maps_params['spn'].split(',')[0]) - 9)
+            a = str(float(self.maps_params['spn'].split(',')[0]) - 5)
             self.maps_params['spn'] = (a + ',' + a)
         self.show_map()
+
     def initUI(self):
-        self.setGeometry(300, 300, 400, 400)
+        self.setGeometry(400, 400, 400, 500)
         self.setWindowTitle('карты')
 
         self.text = QLabel("Координаты:", self)
@@ -88,7 +107,8 @@ class Example(QWidget):
         self.Error.move(20, 140)
 
         self.label = QLabel(self)
-        self.label.move(0,170)
+        self.label.move(10, 160)
+
     def show_st(self):
         self.maps_params['ll'] = ','.join([self.ql1.text(), self.ql2.text()])
         self.maps_params['spn'] = f'{self.mashtab.text()},{self.mashtab.text()}'
@@ -105,7 +125,7 @@ class Example(QWidget):
         else:
             self.map = QPixmap('./data/map.png')
             self.label.setPixmap(self.map)
-            self.label.resize(200, 200)
+            self.label.resize(300, 300)
 
 
 if __name__ == '__main__':
